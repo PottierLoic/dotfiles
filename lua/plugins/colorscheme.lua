6122659489ac60bd149d86cf979fd30333b61c5c
Loïc Pottier <loic.pottier@live.fr>
2023-12-18 11:15:19 +0100

    base conf

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+
+  {
+    'goolord/alpha-nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function ()
+        require'alpha'.setup(require'alpha.themes.startify'.config)
+    end
+  },
+
+  
+}
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,69 @@
+return {
+  'neovim/nvim-lspconfig',
+  event = 'BufReadPre',
+  dependencies = {
+    'folke/neodev.nvim',
+    'mfussenegger/nvim-jdtls',
+    'simrat39/rust-tools.nvim',
+    'jose-elias-alvarez/null-ls.nvim',
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    'alaviss/nim.nvim',
+  },
+  init = function() require('pynappo/keymaps').setup.diagnostics() end,
+  config = function()
+    require('mason').setup({ ui = { border = 'single' } })
+
+
+    require('neodev').setup({})
+    require('mason-tool-installer').setup {
+      ensure_installed = {
+        'codespell',
+        'cpptools',
+        'csharp-language-server',
+        'gopls',
+        -- 'html-lsp',
+        'java-debug-adapter',
+        'java-test',
+        'jdtls',
+        -- 'json-lsp',
+        -- 'ltex-ls',
+        'lua-language-server',
+        'marksman',
+        'netcoredbg',
+        'nimlsp',
+        'powershell-editor-services',
+        'python-lsp-server',
+        'rust-analyzer',
+        -- 'shopify-theme-check',
+        'stylua',
+        'zls',
+      },
+    }
+
+    local mason_lspconfig = require('mason-lspconfig')
+    local lspconfig = require('lspconfig')
+    mason_lspconfig.setup()
+    mason_lspconfig.setup_handlers {
+      function(ls) lspconfig[ls].setup(require('pynappo/lsp/configs')[ls]) end,
+      rust_analyzer = function() require('rust-tools').setup() end,
+      jdtls = function() end, -- use method recommended by nvim-jdtls @ ../../../ftplugin/java.lua
+    }
+
+    local null_ls = require('null-ls')
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.completion.spell.with({
+          filetypes = { 'markdown', 'text' },
+        }),
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.code_actions.gitsigns,
+        null_ls.builtins.hover.dictionary,
+        null_ls.builtins.hover.printenv,
+        null_ls.builtins.formatting.codespell
+      },
+    })
+
+  end,
+}
