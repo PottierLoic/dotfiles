6cf03e3cdf319f42765bfa579e580c2fedc72b18
PottierLoic <loic.pottier@live.fr>
2024-06-17 21:23:44 +0200

    feat: fix discord presence and indent line in dashboard

--- nvim/lua/plugins/indent_blankline.lua
+++ nvim/lua/plugins/indent_blankline.lua
@@ -7,6 +7,7 @@ return {
       tab_char = "|",
     },
     scope = { show_start = false, show_end = false },
+    exclude = { filetypes = { 'dashboard' } },
   },
   config = function(_, opts)
     require("ibl").setup(opts)
