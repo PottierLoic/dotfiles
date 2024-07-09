4fb9400e21fe8f9e162e3bb3db16d50b7ef75f6c
Loïc Pottier <loic.pottier@live.fr>
2024-07-09 15:50:44 +0200

    utils: add rg install

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -102,7 +102,9 @@ return {
 		},
     -- stylua: ignore
 		keys = {
-			{ "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)"	},
+      { "<leader>S", function() require("spectre").toggle() end, desc = "Toggle Spectre" },
+      { "<leader>sw", function() require("spectre").open_visual({select_word=true}) end, desc = "Search current word" },
+      { "<leader>sp", function() require("spectre").open_file_search({select_word=true}) end, desc = "Search on current file" },
 		},
 	},
 }
