e8055e11a80692674f967a03e1cda6245db398a8
PottierLoic <loic.pottier@live.fr>
2024-11-14 00:39:55 +0100

    feat: ensure new wallpaper is not the same as actual

--- waybar/scripts/random_wallpaper.sh
+++ waybar/scripts/random_wallpaper.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
 WALLPAPER_DIR="$HOME/Wallpapers"
+LAST_WALLPAPER_FILE="$HOME/.last_wallpaper"
 
 if ! pgrep -x "swww-daemon" > /dev/null; then
-  echo "Starting swww-daemon..."
   swww init
 fi
 
-if [ -d "$WALLPAPER_DIR" ] && [ "$(ls -A $WALLPAPER_DIR)" ]; then
-  RANDOM_WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
-  
-  echo "Setting wallpaper to $RANDOM_WALLPAPER"
-  swww img "$RANDOM_WALLPAPER" --transition-type grow --transition-duration 1.5
+if [ -d "$WALLPAPER_DIR" ] && [ "$(ls -A "$WALLPAPER_DIR")" ]; then
+  while true; do
+    NEW_WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+    if [ "$NEW_WALLPAPER" != "$(cat "$LAST_WALLPAPER_FILE" 2>/dev/null)" ]; then
+      break
+    fi
+  done
+  echo "Setting wallpaper to $NEW_WALLPAPER"
+  swww img "$NEW_WALLPAPER" --transition-type grow --transition-duration 1.5
+  echo "$NEW_WALLPAPER" > "$LAST_WALLPAPER_FILE"
 else
   echo "Wallpaper directory is empty or does not exist."
   exit 1
 fi
+
