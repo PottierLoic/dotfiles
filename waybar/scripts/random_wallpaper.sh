5373fb218743ef654e3d286eb3b543df7a85800e
PottierLoic <loic.pottier@live.fr>
2024-11-22 21:35:46 +0100

    feat:fix scripts paths

--- waybar/scripts/random_wallpaper.sh
+++ waybar/scripts/random_wallpaper.sh
@@ -18,9 +18,9 @@ if [ -d "$WALLPAPER_DIR" ] && [ "$(ls -A "$WALLPAPER_DIR")" ]; then
   swww img "$NEW_WALLPAPER" --transition-type grow --transition-duration 1.5
   echo "$NEW_WALLPAPER" > "$LAST_WALLPAPER_FILE"
   echo "Generating color palette based in $NEW_WALLPAPER"
-  python generate_colors.py "$NEW_WALLPAPER"
+  python ~/.config/waybar/scripts/generate_colors.py "$NEW_WALLPAPER"
   echo "Updating colors"
-  bash change_colors.sh
+  bash ~/.config/waybar/scripts/change_colors.sh
 else
   echo "Wallpaper directory is empty or does not exist."
   exit 1
